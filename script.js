e9732eb25451f4e70acee1ca5bd23da096a7bcaf
Istiaque Ahmed <131990831+MrIAS1613@users.noreply.github.com>
2025-09-08 18:22:37 +0600

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ---------- Config ----------
 const CONFETTI_DURATION_MS = 10000; // 10s
 const BIRTHDAY_AUDIO_ID = "birthday-audio";
-const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"; // replace with your new deploy URL
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzn8wT-tgQEn3xQn1BJTfpL-B-dDdhw5ZMJQYU3fabNJaScYPXj1lPM-hvKuGJO_A4/exec"; 
 
 // ---------- Menu toggle ----------
 const menuBtn = document.getElementById("menu-btn");
